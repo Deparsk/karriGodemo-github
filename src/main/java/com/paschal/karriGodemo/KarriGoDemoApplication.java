e669fd7a19859e519a76b746570cda0f868cee87
Deparsk <paschal.okpara@decagon.dev>
2023-11-19 10:18:43 +0100

    edited CI-workflow

--- src/main/java/com/paschal/karriGodemo/KarriGoDemoApplication.java
+++ src/main/java/com/paschal/karriGodemo/KarriGoDemoApplication.java
@@ -68,3 +68,10 @@ class Card {
 		this.description = description;
 	}
 }
+//echo "# karriGo-demo-github" >> README.md
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/Deparsk/karriGo-demo-github.git
+//		git push -u origin main
